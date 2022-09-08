6f90bfbc8f014f7f28cf369e36c9b75807cd8579
douglas oliveira <douglasgama0@gmail.com>
2022-09-08 18:22:54 -0300

    feat: adicionando componente Treatments

---
+++ src/frontend/src/pages/Treatments.tsx
@@ -0,0 +1,5 @@
+export default function Treatments() {
+  return (
+    <div>Treatments</div>
+  );
+}
--- src/frontend/src/routes/Routes.tsx
+++ src/frontend/src/routes/Routes.tsx
@@ -2,6 +2,8 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 
 import Login from '../pages/Login';
 import Register from '../pages/Register';
+import Main from '../components/Main';
+import Treatments from '../pages/Treatments';
 
 export default function Router() {
   return (
@@ -9,6 +11,14 @@ export default function Router() {
       <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route
+        path="/treatments"
+        element={(
+          <Main>
+            <Treatments />
+          </Main>
+      )}
+      />
     </Routes>
   );
 }
