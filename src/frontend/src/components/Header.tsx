900bfc72d4d2ffe44452d75e595feedeea2f84ec
douglas oliveira <douglasgama0@gmail.com>
2022-09-08 18:28:14 -0300

    feat: criando header com links para navegação

--- src/frontend/src/components/Header.tsx
+++ src/frontend/src/components/Header.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <nav>
+        <div>
+          <Link
+            to="/register"
+          >
+            Registrar Novo Tratamento
+          </Link>
+          <Link
+            to="/treatments"
+          >
+            Todos os Tratamentos
+          </Link>
+        </div>
+        <div>
+          <Link
+            to="/"
+          >
+            Logout
+          </Link>
+        </div>
+      </nav>
+    </header>
   );
 }
