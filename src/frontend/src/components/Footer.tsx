1a5ceefc23d1e41ab97262bcd7c45cf04f2237c8
douglas oliveira <douglasgama0@gmail.com>
2022-09-08 18:22:11 -0300

    feat: adicionando Footer e Header

---
+++ src/frontend/src/components/Footer.tsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <footer className="footer-page">
+      Desenvolvido por
+      {' '}
+      <a
+        href="https://www.linkedin.com/in/douglas-d-oliveira/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Douglas Oliveira
+      </a>
+    </footer>
+  );
+}
