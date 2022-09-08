fc0474696d9a990383fb196c84d2f0c4d310c496
douglas oliveira <douglasgama0@gmail.com>
2022-09-08 19:57:39 -0300

    feat: adicionando tabela de tratamentos

---
+++ src/frontend/src/mocks/data.ts
@@ -0,0 +1,49 @@
+const mockTreatments = [
+  {
+    id: 1,
+    nomePaciente: 'Carlos Silva',
+    nomeDoTratamento: 'Obturação',
+    formaDePagamento: 'Pix',
+    qntParcelas: 0,
+    dataAtendimento: '01/01/2001',
+    valor: 100,
+  },
+  {
+    id: 2,
+    nomePaciente: 'Matheus Oliveira',
+    nomeDoTratamento: 'Restauração',
+    formaDePagamento: 'Cartão',
+    qntParcelas: 5,
+    dataAtendimento: '01/01/2001',
+    valor: 150,
+  },
+  {
+    id: 3,
+    nomePaciente: 'José Carlos',
+    nomeDoTratamento: 'Canal',
+    formaDePagamento: 'Cartão',
+    qntParcelas: 6,
+    dataAtendimento: '01/01/2001',
+    valor: 400,
+  },
+  {
+    id: 4,
+    nomePaciente: 'Mariana Cavalvante',
+    nomeDoTratamento: 'Implante',
+    formaDePagamento: 'Cartão',
+    qntParcelas: 3,
+    dataAtendimento: '01/01/2001',
+    valor: 600,
+  },
+  {
+    id: 5,
+    nomePaciente: 'José Carlos',
+    nomeDoTratamento: 'Canal',
+    formaDePagamento: 'Cartão',
+    qntParcelas: 6,
+    dataAtendimento: '01/01/2001',
+    valor: 400,
+  },
+];
+
+export default mockTreatments;
--- src/frontend/src/pages/Treatments.tsx
+++ src/frontend/src/pages/Treatments.tsx
@@ -1,10 +1,72 @@
+import { useState, useEffect } from 'react';
+
 import NewTreatments from '../components/NewTreatments';
 
+import mockTreatments from '../mocks/data';
+import { ITreatments } from '../interface/Treatments';
+
 export default function Treatments(): JSX.Element {
+  const [dataTreatments, setDataTreatments] = useState<ITreatments[]>([]);
+  const myBilling: number[] = [0];
+
+  useEffect(() => {
+    setDataTreatments(mockTreatments);
+  }, []);
+
   return (
-    <>
-      <div>Treatments</div>
+    <div>
       <NewTreatments />
-    </>
+      <table>
+        <tbody>
+          <tr>
+            <th>Nome de Paciente</th>
+            <th>Nome do Tratamento</th>
+            <th>Forma de Pagamento</th>
+            <th>Quantidade de parcelas</th>
+            <th>Período de Pagamento</th>
+            <th>Valor Total</th>
+            <th>Ações</th>
+          </tr>
+        </tbody>
+        {
+          dataTreatments.map((el) => {
+            const {
+              id,
+              nomeDoTratamento,
+              nomePaciente,
+              formaDePagamento,
+              qntParcelas,
+              dataAtendimento,
+              valor,
+            } = el;
+            myBilling.push(valor);
+            return (
+              <tbody key={id}>
+                <tr>
+                  <td>{ nomePaciente }</td>
+                  <td>{ nomeDoTratamento }</td>
+                  <td>{ formaDePagamento }</td>
+                  <td>{ qntParcelas }</td>
+                  <td>
+                    { dataAtendimento }
+                    { ' ' }
+                    até
+                    { ' ' }
+                    { dataAtendimento }
+                  </td>
+                  <td>{ valor }</td>
+                  <td>Excluir</td>
+                </tr>
+              </tbody>
+            );
+          })
+        }
+      </table>
+      <h3>
+        Valor Total: R$
+        { ' ' }
+        { myBilling.reduce((prev, curr) => prev + curr) }
+      </h3>
+    </div>
   );
 }
