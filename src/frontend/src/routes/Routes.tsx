36a36eef0524852aca2b249b721fe5e91623bc2e
douglas oliveira <douglasgama0@gmail.com>
2022-09-09 12:22:40 -0300

    fix: ajustando rotas da aplicação

--- src/frontend/src/routes/Routes.tsx
+++ src/frontend/src/routes/Routes.tsx
@@ -1,7 +1,6 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
 
 import Login from '../pages/Login';
-import Register from '../pages/Register';
 import Main from '../components/Main';
 import Treatments from '../pages/Treatments';
 
@@ -10,7 +9,6 @@ export default function Router() {
     <Routes>
       <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
       <Route
         path="/treatments"
         element={(
