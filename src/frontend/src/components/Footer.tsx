99ad6eb7c01d652cfcee89271c64d7c00a49c1d1
douglas oliveira <douglasgama0@gmail.com>
2022-09-10 11:31:35 -0300

    styled: estilizando footer

--- src/frontend/src/components/Footer.tsx
+++ src/frontend/src/components/Footer.tsx
@@ -1,15 +1,17 @@
+import { FooterContainer, LinkFooter } from '../styles/Footer';
+
 export default function Footer(): JSX.Element {
   return (
-    <footer className="footer-page">
+    <FooterContainer className="footer-page">
       Desenvolvido por
       {' '}
-      <a
+      <LinkFooter
         href="https://www.linkedin.com/in/douglas-d-oliveira/"
         target="_blank"
         rel="noreferrer"
       >
         Douglas Oliveira
-      </a>
-    </footer>
+      </LinkFooter>
+    </FooterContainer>
   );
 }
